06169d73d8e7a431be62801cab425f522d92615b
Ahmed ElArabawy <aelarabawy@aelarabawy-VirtualBox.(none)>
2014-11-18 15:11:44 -0800

    Adding The Introduction project for the Raspberry Pi

---
+++ rpi-projects/intro-proj/src/hello_world.c
@@ -0,0 +1,13 @@
+/*******
+ * hello-world.c
+ * Test program to test the cross toolchain for the Raspberry Pi
+ * Ahmed ElArabawy
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf ("Hello, Raspberry Pi Cross-compilation world !\n");
+    return 0;
+}
