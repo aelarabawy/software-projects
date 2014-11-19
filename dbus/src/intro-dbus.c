4fbaacc79a526d5b195408cc7f2d4fb08cd3a40a
Ahmed ElArabawy <aelarabawy.git@lasilka.com>
2014-11-19 00:23:35 -0800

    Added Support for Owning and Watching Bus Names, working fine The application can run now as either a client or a server

--- dbus/src/intro-dbus.c
+++ dbus/src/intro-dbus.c
@@ -9,31 +9,54 @@
 #include <string.h>
 #include <gio/gio.h>
 
-void dbus_acquiredCallBack (GDBusConnection *connection,
-		                    const gchar *name,
-							gpointer userData) {
-	printf("Entering the function dbus_acquiredCallBack() \n");
-	printf("Name is %s\n",name);
+
+enum OperationMode {
+	OPER_MODE_INVALID = 0,
+	OPER_MODE_CLIENT = 1,
+	OPER_MODE_SERVER = 2
+};
+
+void dbus_usage (void) {
+	//Print the usage of the command line
+	//TODO Complete this function
 }
 
-void dbus_nameAcquiredCallBack (GDBusConnection *connection,
-		                        const gchar *name,
-					   		    gpointer userData) {
+void waitForEvents(void) {
+    GMainLoop * loop;
+    loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop);
+}
+
+static void dbus_acquiredCallBack (GDBusConnection *connection,
+        		                   const gchar *name,
+							       gpointer userData) {
+	printf("Entering the function dbus_acquiredCallBack() \n");
+	printf("---- Name is %s\n",name);
+	printf("---- Connection is %p\n", connection);
+
+	//The server will need to export the objects here (g_dbus_connection_register_object)
+}
+
+static void dbus_nameAcquiredCallBack (GDBusConnection *connection,
+		                               const gchar *name,
+					   		           gpointer userData) {
 	printf("Entering the function dbus_nameAcquiredCallBack() \n");
 	printf("Name is %s\n",name);
+	printf("---- Connection is %p\n", connection);
 }
 
-void dbus_nameLostCallBack (GDBusConnection *connection,
-		                    const gchar *name,
-							gpointer userData) {
+static void dbus_nameLostCallBack (GDBusConnection *connection,
+		                           const gchar *name,
+							       gpointer userData) {
 	printf("Entering the function dbus_nameLostCallBack() \n");
 	printf("Name is %s\n",name);
+	printf("---- Connection is %p\n", connection);
+
+	//The server will need to de-register the objects here
 }
 
-
-
-guint dbus_ownName (char* busName){
-	printf ("Entering dbus_ownName, busName = %s\n", busName);
+guint dbus_reserveBusName (char* busName){
+	printf ("Entering dbus_reserveBusName, busName = %s\n", busName);
 
 	guint ownerId;
 	ownerId  = g_bus_own_name (G_BUS_TYPE_SESSION,
@@ -44,60 +67,114 @@ guint dbus_ownName (char* busName){
 							   dbus_nameLostCallBack,
 							   NULL,
 							   NULL);
+	printf ("dbus_ownName: ownerId = %d \n", ownerId);
 
 	return ownerId;
 }
 
-void dbus_unownName (guint ownerId) {
-	printf ("Entering the dbus_unownName, ownerId = %d\n", ownerId);
+void dbus_releaseBusName (guint ownerId) {
+	printf ("Entering the dbus_releaseBusName, ownerId = %d\n", ownerId);
 	g_bus_unown_name(ownerId);
 }
 
-void dbus_watchName (char* busName){
-	printf ("Entering dbus_watchName, busName = %s\n", busName);
+/*****************************************
+ * dbus_watchName ()
+ * This function is used by the client to watch for a name on the bus
+ * It takes the busName to watch
+ * */
+
+static void dbus_nameUpCallBack (GDBusConnection *connection,
+		                         const gchar * busName,
+		 		   		         const gchar * nameOwner,
+						         gpointer userData) {
+	printf ("Entering dbus_nameUpCallBack function\n");
+    printf ("----- BusName = %s \n", busName);
+	printf ("----- Owner Unique Name = %s \n", nameOwner);
+	printf ("----- Connection = %p \n", connection);
+
+	//The Client should create object proxies here
 	return;
 }
 
-//main function
-void main (int argc, char** argv) {
-	char command [100]="";
-	char* token;
-	guint ownerId = 0;
+static void dbus_nameDownCallBack (GDBusConnection *connection,
+		                           const gchar * busName,
+				       	  	       gpointer userData) {
+	printf ("Entering dbus_nameDownCallBack function\n");
+    printf ("----- BusName = %s \n", busName);
+	printf ("----- Connection = %p \n", connection);
 
-	while (strcmp(command,"exit") != 0) {
-		printf("> ");
-		fgets(command, sizeof (command), stdin);
-
-		//Remove the Terminating EOL
-		strtok(command,"\n");
-
-		//Get the command
-		token = strtok(command, " ");
-		if (strcmp(token,"own_name") == 0) {
-			//Get the Name to be owned
-			token = strtok(NULL," ");
-			if (token != NULL){
-				ownerId = dbus_ownName(token);
-			} else {
-				printf("Invalid Entry, BusName needs to be provided\n");
-			}
-      } else if (strcmp(token,"unown_name") == 0) {
-			if (ownerId != 0){
-				dbus_unownName(ownerId);
-				ownerId = 0;
-			} else {
-				printf("There is no Name to release \n");
-			}
-      } else if (strcmp(token,"watch_name") == 0){
-  	  //Get the Name to be watched
-			token = strtok(NULL," ");
-			if (token != NULL){
-				dbus_watchName(token);
-			} else {
-				printf("Invalid Entry, BusName needs to be provided\n");
-			}
-      }
-	}
+	//The Client should destroy object proxies here
 
 	return;
+}
+
+
+guint dbus_watchBusName (char* busName){
+	printf ("Entering dbus_watchName, busName = %s\n", busName);
+
+	guint retVal;
+	retVal = g_bus_watch_name (G_BUS_TYPE_SESSION,
+                               busName,
+			                   G_BUS_NAME_WATCHER_FLAGS_NONE,
+							   dbus_nameUpCallBack,
+							   dbus_nameDownCallBack,
+			                   NULL,
+			                   NULL);
+	return retVal;
+}
+
+void dbus_stopWatchingBusName (guint busNameId){
+	printf ("Entering dbus_stopWatchingBusName, busNameId = %d\n", busNameId);
+
+	g_bus_unwatch_name (busNameId);
+}
+
+//main function
+int main (int argc, char* argv[]) {
+
+	//First Read the passed Command Line Arguments
+	//TODO need to use a more systematic approach to parse passed arguments
+	//     Check with the file gdbus-example-own-name.c and the use of GOptionEntry
+	//     After you fix it, add it to the garden roses
+    //TODO I need to build a logger, and use it instead of the printfs
+	//     It should have severity level, function name, line number, ...
+	//TODO I need to separate functionality in different files
+	unsigned int operationMode;
+	char busName [100];
+	guint busNameId = 0;
+
+	if (argc < 2){
+		printf ("Invalid Entry ...\n");
+		dbus_usage();
+		return -1;
+	} else {
+		if (strcmp(argv[1], "--server") == 0){
+			operationMode = OPER_MODE_SERVER;
+			strcpy (busName, argv[2]);
+		} else if (strcmp(argv[1], "--client") == 0) {
+			operationMode = OPER_MODE_CLIENT;
+			strcpy (busName, argv[2]);
+		} else {
+			operationMode = OPER_MODE_INVALID;
+			printf ("Invalid Operation Mode\n");
+			dbus_usage();
+			return -1;
+		}
+	}
+
+	//Now let us start operation
+	switch (operationMode) {
+	case OPER_MODE_CLIENT:
+		busNameId = dbus_watchBusName (busName);
+		waitForEvents();
+		break;
+
+	case OPER_MODE_SERVER:
+		printf("Trying to own the name : %s \n", busName);
+        busNameId = dbus_reserveBusName(busName);
+		waitForEvents();
+		break;
+	}
+
+	return 0;
 }
